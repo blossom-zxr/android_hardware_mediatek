8ce451c57a2d4bac1690e29ca249b20aeee35873
fukiame <fukiame@proton.me>
2026-02-28 21:51:38 +0700

    libmtkperf_client: demote logging to verbose
    Signed-off-by: fukiame <fukiame@proton.me>

--- libmtkperf_client/client.c
+++ libmtkperf_client/client.c
@@ -9,7 +9,7 @@
 #include <log/log.h>
 
 int perf_lock_acq(int hdl, int dur, long num, unsigned int tid) {
-    ALOGI("[%s] hdl: %d, dur: %d, num: %ld, tid: %u",
+    ALOGV("[%s] hdl: %d, dur: %d, num: %ld, tid: %u",
             __func__, hdl, dur, num, tid);
     if (hdl > 0)
         return hdl;
@@ -18,11 +18,11 @@ int perf_lock_acq(int hdl, int dur, long num, unsigned int tid) {
 }
 
 int perf_lock_rel(int hdl) {
-    ALOGI("[%s] hdl: %d", __func__, hdl);
+    ALOGV("[%s] hdl: %d", __func__, hdl);
     return 0;
 }
 
 int perf_cus_lock_hint(int hint, int dur) {
-    ALOGI("[%s], hint: %d, dur: %d", __func__, hint, dur);
+    ALOGV("[%s], hint: %d, dur: %d", __func__, hint, dur);
     return 233;
 }
